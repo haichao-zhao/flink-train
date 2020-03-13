a681f9111ad6962dfb44f8ebe6ccacb31e377e58
haichao-zhao <810871100@qq.com>
2020-03-13 21:41:46 +0800

    Flink  Windows ProcessWindowsFunction 函数 API，Java&Scala实现

--- src/main/java/com/zhc/flink/course07/JavaWindowsApp.java
+++ src/main/java/com/zhc/flink/course07/JavaWindowsApp.java
@@ -2,10 +2,14 @@ package com.zhc.flink.course07;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 
 public class JavaWindowsApp {
@@ -18,15 +22,15 @@ public class JavaWindowsApp {
 
 //        tumblingWindowsFunc(data);
 
-        windowsReduceFunc(data);
+//        windowsReduceFunc(data);
+        windowsProcessFunc(data);
         env.execute("JavaWindowsApp");
 
     }
 
-    //滚动窗口方式处理
-    private static void tumblingWindowsFunc(DataStreamSource<String> data) {
 
-        data.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+    private static SingleOutputStreamOperator<Tuple2<String, Integer>> getTestWindowData(DataStreamSource<String> data) {
+        SingleOutputStreamOperator<Tuple2<String, Integer>> operator = data.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
             @Override
             public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
 
@@ -35,7 +39,29 @@ public class JavaWindowsApp {
                     out.collect(new Tuple2<>(s, 1));
                 }
             }
-        }).keyBy(0)
+        });
+        return operator;
+    }
+
+
+    private static SingleOutputStreamOperator<Tuple2<Integer, Integer>> getTestWindowsFunctionData(DataStreamSource<String> data) {
+        SingleOutputStreamOperator<Tuple2<Integer, Integer>> operator = data.flatMap(new FlatMapFunction<String, Tuple2<Integer, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<Integer, Integer>> out) throws Exception {
+
+                String[] strings = value.split(" ");
+                for (String s : strings) {
+                    out.collect(new Tuple2<>(1, Integer.parseInt(s)));
+                }
+            }
+        });
+        return operator;
+    }
+
+    //滚动窗口方式处理
+    private static void tumblingWindowsFunc(DataStreamSource<String> data) {
+
+        getTestWindowData(data).keyBy(0)
                 .timeWindow(Time.seconds(5))
                 .sum(1)
                 .print()
@@ -46,41 +72,44 @@ public class JavaWindowsApp {
     //滑动窗口方式处理
     private static void slidingWindowsFunc(DataStreamSource<String> data) {
 
-        data.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
-            @Override
-            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
-
-                String[] strings = value.split(" ");
-                for (String s : strings) {
-                    out.collect(new Tuple2<>(s, 1));
-                }
-            }
-        }).keyBy(0)
+        getTestWindowData(data).keyBy(0)
                 .timeWindow(Time.seconds(10), Time.seconds(5))
                 .sum(1)
                 .print()
                 .setParallelism(1);
     }
 
-    ////窗口函数：Reduce
+    //窗口函数：Reduce
     private static void windowsReduceFunc(DataStreamSource<String> data) {
 
-        data.flatMap(new FlatMapFunction<String, Tuple2<Integer, Integer>>() {
-            @Override
-            public void flatMap(String value, Collector<Tuple2<Integer, Integer>> out) throws Exception {
-
-                String[] strings = value.split(" ");
-                for (String s : strings) {
-                    out.collect(new Tuple2<>(1, Integer.parseInt(s)));
-                }
-            }
-        }).keyBy(0)
+        getTestWindowsFunctionData(data).keyBy(0)
                 .timeWindow(Time.seconds(5))
                 .reduce(new ReduceFunction<Tuple2<Integer, Integer>>() {
                     @Override
                     public Tuple2<Integer, Integer> reduce(Tuple2<Integer, Integer> v1, Tuple2<Integer, Integer> v2) throws Exception {
                         System.out.println("v1 = [" + v1 + "], v2 = [" + v2 + "]");
                         return new Tuple2<>(v1.f0, v1.f1 + v2.f1);
+                    }
+                })
+                .print()
+                .setParallelism(1);
+    }
+
+
+    //窗口函数：Process
+    private static void windowsProcessFunc(DataStreamSource<String> data) {
+
+        getTestWindowsFunctionData(data).keyBy(0)
+                .timeWindow(Time.seconds(5))
+                .process(new ProcessWindowFunction<Tuple2<Integer, Integer>, Object, Tuple, TimeWindow>() {
+                    @Override
+                    public void process(Tuple tuple, Context context, Iterable<Tuple2<Integer, Integer>> input, Collector<Object> out) throws Exception {
+                        System.out.println("----------------");
+                        long count = 0;
+                        for (Tuple2<Integer, Integer> in : input) {
+                            count++;
+                        }
+                        out.collect("Window: " + context.window() + "count: " + count);
                     }
                 })
                 .print()
--- src/main/scala/com/zhc/flink/course07/WindowsApp.scala
+++ src/main/scala/com/zhc/flink/course07/WindowsApp.scala
@@ -1,9 +1,13 @@
 package com.zhc.flink.course07
 
 
+import org.apache.flink.api.java.tuple.Tuple
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
 
 object WindowsApp {
   def main(args: Array[String]): Unit = {
@@ -14,8 +18,8 @@ object WindowsApp {
 
     //    tumblingWindowsFunc(data)
 
-    windowsReduceFunc(data)
-
+    //    windowsReduceFunc(data)
+    windowsProcessFunc(data)
     env.execute("WindowsApp")
   }
 
@@ -50,5 +54,26 @@ object WindowsApp {
         (x._1, x._2 + y._2)
       })
       .print()
+  }
+
+  //窗口函数：Process
+  private def windowsProcessFunc(data: DataStream[String]) = {
+    data.flatMap(_.split(" "))
+      .map(x => (1, x.toInt))
+      .keyBy(0)
+      .timeWindow(Time.seconds(5))
+      .process(new MyProcessWindowFunction())
+      .print()
+  }
+}
+
+class MyProcessWindowFunction extends ProcessWindowFunction[(Int, Int), String, Tuple, TimeWindow] {
+
+  override def process(key: Tuple, context: Context, input: Iterable[(Int, Int)], out: Collector[String]): Unit = {
+    var count = 0L
+    for (in <- input) {
+      count = count + 1
+    }
+    out.collect(s"Window ${context.window} count: $count")
   }
 }
